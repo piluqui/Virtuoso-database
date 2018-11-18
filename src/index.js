0713cf4d0e74fccad6ef6a0af9f7b009a5ad3c78
piluqui <piluqui@gmail.com>
2018-11-17 23:48:38 -0300

    Test package Sparql

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const {Client} = require('virtuoso-sparql-client');
+//const {Client} = require('virtuoso-sparql-client');
 
 //Setting
 app.set('port', 3000);
@@ -26,15 +26,22 @@ app.listen(app.get('port'), () => {
 
 //client virtuoso
 
+sparql = require ('sparql');
+client = new sparql.Client ('http://dbpedia.org/sparql');
+client.query ('select * where { ?s ?p ?o } limit 100'), (err, res) ->
+  console.log (res);
+
  
-const DbPediaClient = new Client('http://192.168.0.22:8890/sparql');
-DbPediaClient.query('DESCRIBE <http://192.168.0.22:8890>')
-  .then((results) => {
-    console.log(results);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// const DbPediaClient = new Client('http://192.168.0.22:8890/sparql');
+// DbPediaClient.query('SELECT DISTINCT  ?o <http://192.168.0.22:8890/DAV/home/dba/rdf_sink/> WHERE:name ?o}')
+//   .then((results) => {
+//     console.log(results);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
   
-  //http://192.168.0.22:8890/sparql?query=SELECT%20DISTINCT%20%20?o%20%3Chttp://localhost:8890/DAV/home/dba/rdf_sink/%3E%20WHERE%20{?s%20foaf:name%20?o}
+//http://192.168.0.22:8890/sparql?query=SELECT%20DISTINCT%20%20?o%20%3Chttp://localhost:8890/DAV/home/dba/rdf_sink/%3E%20WHERE%20{?s%20foaf:name%20?o}
+
+//query=SELECT DISTINCT  ?o <http://localhost:8890/DAV/home/dba/rdf_sink/> WHERE:name ?o}" http://localhost:8890/sparql
